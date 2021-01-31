3f3b979b26871cc00425c4bb834cfafcbac1bf8f
Yusuke Nojima <nojima@ynojima.com>
2021-01-31 22:34:12 +0900

    ch02

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod vec3;
+use vec3::*;
+
 fn write_header(nx: i32, ny: i32) {
     println!("P3");
     println!("{} {}", nx, ny);
@@ -7,12 +10,14 @@ fn write_header(nx: i32, ny: i32) {
 fn write_body(nx: i32, ny: i32) {
     for y in (0..ny).rev() {
         for x in 0..nx {
-            let r = (x as f32) / (nx as f32);
-            let g = (y as f32) / (ny as f32);
-            let b = 0.2;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let color = Vec3::new(
+                (x as f32) / (nx as f32),
+                (y as f32) / (ny as f32),
+                0.2
+            );
+            let ir = (255.99 * color.x) as i32;
+            let ig = (255.99 * color.y) as i32;
+            let ib = (255.99 * color.z) as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
---
+++ src/vec3.rs
@@ -0,0 +1,133 @@
+use std::ops;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Self::Output {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: Vec3) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
+impl ops::Div<Vec3> for f32 {
+    type Output = Vec3;
+    fn div(self, rhs: Vec3) -> Self::Output {
+        Vec3 {
+            x: self / rhs.x,
+            y: self / rhs.y,
+            z: self / rhs.z,
+        }
+    }
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn length(self) -> f32 {
+        self.dot(self).sqrt()
+    }
+
+    pub fn normalize(self) -> Vec3 {
+        self / self.length()
+    }
+
+    pub fn dot(self, rhs: Vec3) -> f32 {
+        self.x*rhs.x + self.y*rhs.y + self.z*rhs.z
+    }
+
+    pub fn cross(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.y*rhs.z - self.z*rhs.y,
+            y: self.z*rhs.x - self.x*rhs.z,
+            z: self.x*rhs.y - self.y*rhs.x,
+        }
+    }
+}
